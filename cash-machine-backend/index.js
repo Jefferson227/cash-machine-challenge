5d40c986b0a664b7d6c1123db88528e5d223d7ac
Jefferson Matheus <jefferson227@gmail.com>
2018-03-28 16:36:03 -0300

    chore(index): add comments

--- cash-machine-backend/index.js
+++ cash-machine-backend/index.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 3001;
 
 const cashMachine = require('./components/cash-machine/cash-machine');
 
+// Starting the server
 app.listen(port, () => {
     console.log('Server running');
 });
 
+// Creating the route for the withdraw function
 app.get('/withdraw/:amount', (req, res) => {
     const amount = req.params.amount.toLowerCase() === 'null'
         ? null
